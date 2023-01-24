9c14b641a3bc648e8b44441a65255b7c1860229e
pyuldashev912 <www.parviz98@ya.ru>
2023-01-24 17:32:17 +0500

    Added config for server

---
+++ internal/app/apiserver/config.go
@@ -0,0 +1,12 @@
+package apiserver
+
+type Config struct {
+	BindAddr    string `toml:"bind_addr"`
+	LogLevel    string `toml:"log_level"`
+	DatabaseURL string `toml:"database_url"`
+}
+
+// NewConfig return new Config instance
+func NewConfig() *Config {
+	return &Config{}
+}
