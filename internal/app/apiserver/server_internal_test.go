f50f4987f372650a3ce382e7a32b2dfa6a72b3b0
pyuldashev912 <www.parviz98@ya.ru>
2023-01-30 16:28:09 +0500

    Added tests for /user routes

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -2,11 +2,14 @@ package apiserver
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
 	"github.com/pyuldashev912/todoapp/internal/app/model"
 	"github.com/pyuldashev912/todoapp/internal/app/store/teststore"
@@ -105,6 +108,117 @@ func TestServer_handleUserLogin(t *testing.T) {
 			buf := &bytes.Buffer{}
 			json.NewEncoder(buf).Encode(tc.payload)
 			req, _ := http.NewRequest(http.MethodPost, "/user/login", buf)
+			srv.ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Result().StatusCode)
+		})
+	}
+}
+
+func TestServer_authUserMW(t *testing.T) {
+	store := teststore.New()
+	u := model.TestUser(t)
+	store.User().Create(u)
+
+	testCases := []struct {
+		name         string
+		cookieValue  map[interface{}]interface{}
+		expectedCode int
+	}{
+		{
+			name: "authenticated",
+			cookieValue: map[interface{}]interface{}{
+				"user_id": u.ID,
+			},
+			expectedCode: http.StatusOK,
+		},
+		{
+			name:         "not authenticated",
+			cookieValue:  nil,
+			expectedCode: http.StatusUnauthorized,
+		},
+	}
+
+	secretKey := []byte("secret")
+	s := newServer(store, sessions.NewCookieStore(secretKey))
+	sc := securecookie.New(secretKey, nil)
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodGet, "/", nil)
+			cookieStr, _ := sc.Encode(sessionName, tc.cookieValue)
+			req.Header.Set("Cookie", fmt.Sprintf("%s=%s", sessionName, cookieStr))
+			s.authUserMW(handler).ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Code)
+		})
+	}
+}
+
+func TestServer_handleUserLogout(t *testing.T) {
+	store := teststore.New()
+	user := model.TestUser(t)
+	store.User().Create(user)
+
+	testCases := []struct {
+		name        string
+		cookieValue map[interface{}]interface{}
+	}{
+		{
+			name: "logged out",
+			cookieValue: map[interface{}]interface{}{
+				"user_id": user.ID,
+			},
+		},
+	}
+
+	secretKey := []byte("secret")
+	s := newServer(store, sessions.NewCookieStore(secretKey))
+	sc := securecookie.New(secretKey, nil)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodGet, "/", nil)
+			cookieStr, _ := sc.Encode(sessionName, tc.cookieValue)
+			req.Header.Set("Cookie", fmt.Sprintf("%s=%s", sessionName, cookieStr))
+			s.handleUserLogout().ServeHTTP(rec, req)
+			assert.Empty(t, rec.Result().Header.Get("Cookie"))
+		})
+	}
+}
+
+func TestServer_handleWhoAmI(t *testing.T) {
+	store := teststore.New()
+	user := model.TestUser(t)
+	store.User().Create(user)
+
+	testCases := []struct {
+		name         string
+		user_id      interface{}
+		cookieValue  map[interface{}]interface{}
+		expectedCode int
+	}{
+		{
+			name:         "authorized",
+			user_id:      user.ID,
+			expectedCode: http.StatusOK,
+		},
+		{
+			name:         "unauthorized",
+			user_id:      45,
+			expectedCode: http.StatusUnauthorized,
+		},
+	}
+
+	srv := newServer(store, nil)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodGet, "/user/whoami", nil)
+			req = req.WithContext(context.WithValue(req.Context(), ctxKeyUser, tc.user_id))
 			srv.ServeHTTP(rec, req)
 			assert.Equal(t, tc.expectedCode, rec.Result().StatusCode)
 		})
