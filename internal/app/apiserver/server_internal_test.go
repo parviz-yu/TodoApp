2e26a3f648119be3cdd88e93b0b434af23f7639c
pyuldashev912 <www.parviz98@ya.ru>
2023-01-31 00:07:08 +0500

    Added tests for for the "/task" route

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -224,3 +224,142 @@ func TestServer_handleWhoAmI(t *testing.T) {
 		})
 	}
 }
+
+func TestServer_handleTaskCreate(t *testing.T) {
+	store := teststore.New()
+	task := model.TestTask(t)
+	srv := newServer(store, nil)
+
+	testCases := []struct {
+		name         string
+		user_id      interface{}
+		payload      map[string]string
+		expectedCode int
+	}{
+		{
+			name:    "valid",
+			user_id: task.UserID,
+			payload: map[string]string{
+				"title":       task.Title,
+				"description": task.Description,
+			},
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:    "invalid title",
+			user_id: task.UserID,
+			payload: map[string]string{
+				"title":       "",
+				"description": task.Description,
+			},
+			expectedCode: http.StatusUnprocessableEntity,
+		},
+		{
+			name:    "invalid description",
+			user_id: task.UserID,
+			payload: map[string]string{
+				"title":       task.Title,
+				"description": "",
+			},
+			expectedCode: http.StatusUnprocessableEntity,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			buf := &bytes.Buffer{}
+			json.NewEncoder(buf).Encode(tc.payload)
+			req, _ := http.NewRequest(http.MethodPost, "/task/add", buf)
+			req = req.WithContext(context.WithValue(req.Context(), ctxKeyUser, tc.user_id))
+			srv.ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Result().StatusCode)
+		})
+	}
+}
+
+func TestServer_handleTaskDelete(t *testing.T) {
+	store := teststore.New()
+	task := model.TestTask(t)
+	store.Task().Create(task)
+	srv := newServer(store, nil)
+
+	testCases := []struct {
+		name         string
+		user_id      interface{}
+		queryString  string
+		expectedCode int
+	}{
+		{
+			name:         "valid id",
+			user_id:      task.UserID,
+			queryString:  "/task/delete?id=1",
+			expectedCode: http.StatusOK,
+		},
+		{
+			name:         "invalid id",
+			user_id:      task.UserID,
+			queryString:  "/task/delete?id=asdas",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "not existing id",
+			user_id:      task.UserID,
+			queryString:  "/task/delete?id=564",
+			expectedCode: http.StatusNotFound,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodDelete, tc.queryString, nil)
+			req = req.WithContext(context.WithValue(req.Context(), ctxKeyUser, tc.user_id))
+			srv.ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Result().StatusCode)
+		})
+	}
+}
+
+func TestServer_handleTaskDone(t *testing.T) {
+	store := teststore.New()
+	task := model.TestTask(t)
+	store.Task().Create(task)
+	srv := newServer(store, nil)
+
+	testCases := []struct {
+		name         string
+		user_id      interface{}
+		queryString  string
+		expectedCode int
+	}{
+		{
+			name:         "valid id",
+			user_id:      task.UserID,
+			queryString:  "/task/done?id=1",
+			expectedCode: http.StatusOK,
+		},
+		{
+			name:         "invalid id",
+			user_id:      task.UserID,
+			queryString:  "/task/done?id=asdas",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name:         "not existing id",
+			user_id:      task.UserID,
+			queryString:  "/task/done?id=564",
+			expectedCode: http.StatusNotFound,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodPatch, tc.queryString, nil)
+			req = req.WithContext(context.WithValue(req.Context(), ctxKeyUser, tc.user_id))
+			srv.ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Result().StatusCode)
+		})
+	}
+}
